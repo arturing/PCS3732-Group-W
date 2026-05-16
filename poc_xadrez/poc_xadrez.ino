c4a1a56e8c95a9b1181bc83e931932bc91cbcf17
random sir <85718555+random-sir@users.noreply.github.com>
2026-05-16 16:25:53 -0300

    Add files via upload

---
+++ poc_xadrez/poc_xadrez.ino
@@ -0,0 +1,59 @@
+#include <string.h>
+#define LINE_PIN_START 6
+#define COLUMN_PIN_START 2
+#define NUMBER_LINES 8
+#define NUMBER_COLUMNS 2
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  for (int columns = COLUMN_PIN_START; columns < NUMBER_COLUMNS; columns++)
+  {
+    pinMode(COLUMN_PIN_START, INPUT);
+  }
+}
+
+bool matrix_prev[NUMBER_LINES][NUMBER_COLUMNS];
+bool matrix[NUMBER_LINES][NUMBER_COLUMNS];
+
+void loop()
+{
+
+  for (int line = 0; line < NUMBER_LINES; line++)
+  {
+    int line_pin = line + LINE_PIN_START;
+    pinMode(line_pin, OUTPUT);
+    digitalWrite(line_pin, HIGH);
+	
+    for (int column = 0; column < NUMBER_COLUMNS; column++){
+    	matrix[line][column] = digitalRead(column+COLUMN_PIN_START);
+    }
+
+
+    pinMode(line_pin, INPUT);
+  }
+
+  int diff = memcmp(matrix, matrix_prev, sizeof(matrix));
+
+  if (diff != 0)
+  {
+    for (int i = 0; i < NUMBER_LINES; i++)
+    {
+      for (int j= 0; j < NUMBER_COLUMNS; j++){
+        if (matrix[i][j] != matrix_prev[i][j])
+        {
+          Serial.print("Button index ");
+          Serial.print(i);
+          Serial.print(",");
+          Serial.print(j);
+          Serial.println(" changed state.");
+        }
+      }
+    }
+    memcpy(matrix_prev, matrix, sizeof(matrix));
+  }
+
+  delay(10);
+}
