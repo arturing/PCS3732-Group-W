d78a0b0a89af43173c5cc65d237cbfd938833d9d
Betoh-20 <betojun@hotmail.com>
2026-05-20 14:26:18 -0300

    feat: remove blinking yellow

--- experimento1/Blink/Blink/Blink.ino
+++ experimento1/Blink/Blink/Blink.ino
@@ -1,19 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 
 #define RGB_LED_PIN 8
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(1, RGB_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200);
   pixels.begin();
-
-  for (int i = 1; i < 7; i++) 
-  {
-    int val = 255 * (i%2);
-    pixels.setPixelColor(RGB_LED_PIN, pixels.Color(val, val, 0));
-    pixels.show(); 
-    delay(500);
-  }
 }
 
 void loop() {
